7512845514aa2af6b7e4ef36207036445bf3ec68
Matthew Hayden <matthayden09@gmail.com>
2021-02-07 15:13:37 -0600

    Set up initial inquirer prompt and function layouts

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cTable = require('console.table');
 
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'employee_tracker_db'
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employee_tracker_db'
 });
- 
+
 connection.connect(err => {
     if (err) throw err
     console.log(`Connect to mysql on thread ${connection.threadId}`)
@@ -25,5 +25,128 @@ connection.connect(err => {
 //     }
 //   ]);
 
+function employeeTracker() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all departments",
+                "View all roles",
+                "Add new employee",
+                "Add new department",
+                "Add new role",
+                "Update employee role",
+                "Exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all employees":
+                    viewEmployees();
+                    break;
 
-  
+                case "View all departments":
+                    viewDepartments();
+                    break;
+
+                case "View all roles":
+                    viewRoles();
+                    break;
+
+                case "Add new employee":
+                    addEmployee();
+                    break;
+
+                case "Add new department":
+                    addDepartment();
+                    break;
+
+                case "Add new role":
+                    addRole();
+                    break;
+
+                case "Update employee role":
+                    updateRole();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function viewEmployees() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+function viewDepartments() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+function viewRoles() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+function addEmployee() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+function addRole() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+function updateRole() {
+    inquirer
+        .prompt({
+            // prompt
+        })
+        .then(function (answer) {
+            // then
+        })
+}
+
+employeeTracker();
