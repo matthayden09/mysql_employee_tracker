57924a604436ad585731ab7f4d21522748e14f04
Matthew Hayden <matthayden09@gmail.com>
2021-02-08 10:09:50 -0600

    updated schema

--- schema.sql
+++ schema.sql
@@ -11,12 +11,12 @@ PRIMARY KEY (id)
 CREATE TABLE employee_role (
 id INT(100) NOT NULL AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL,
--- salary DECIMAL(5,2) NOT NULL,
--- department_id INT(100) NOT NULL,
+salary DECIMAL(8,0) NULL,
+department_id INT(100) NOT NULL,
 PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
 id INT(100) NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
@@ -25,13 +25,28 @@ manager_id INT(100) NULL,
 PRIMARY KEY (id)
 );
 
-INSERT INTO department (department_name) VALUES ("Logistics");
+-- Department table 
 INSERT INTO department (department_name) VALUES ("Engineering");
-INSERT INTO department (department_name) VALUES ("Human Resources");
+INSERT INTO department (department_name) VALUES ("Logistics");
+INSERT INTO department (department_name) VALUES ("Maintenance");
 
-INSERT INTO employee_role (title) VALUES ("QA Analyst");
--- INSERT INTO employee_role (salary) VALUES ("40,000");
--- INSERT INTO employee_role (department_id) VALUES ("1");
+-- Employee role table 
+INSERT INTO employee_role(title, salary, department_id)
+VALUES ("QA Analyst", "40000", "1");
+INSERT INTO employee_role(title, salary, department_id)
+VALUES ("Project Manager", "120000", "2");
+INSERT INTO employee_role(title, salary, department_id)
+VALUES ("Janitor", "50000", "3");
+
+-- Employees table
+INSERT INTO employees(first_name, last_name, role_id)
+VALUES ("Matt", "Hayden", "1");
+INSERT INTO employees(first_name, last_name, role_id)
+VALUES ("Nicole", "Roberts", "2");
+INSERT INTO employees(first_name, last_name, role_id)
+VALUES ("Bob", "Baultrus", "3");
+
 
 SELECT * FROM department;
 SELECT * FROM employee_role;
+SELECT * FROM employees;
